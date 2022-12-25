83a3b66a5c6ffcb635b2286ec8ad3b6290030d1c
Noreenmotaz <121252591+Noreenmotaz@users.noreply.github.com>
2022-12-25 22:55:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter  the lower bound:");
+            int n1 = int .Parse(Console.ReadLine());
+            Console.WriteLine("Enter  the upper bound:");
+            int n2 = int.Parse(Console.ReadLine());
+            
+            
+            for (int i =n1; i <= n2; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i%j==0)
+                    {
+                        sum+=j; 
+                    }
+                }
+
+                if (sum == i)
+                {
+                    Console.WriteLine(i+"is a perfect number");
+                }
+
+                    
+                
+            }
+           
+
+            
+        }
+    }
+}
---
+++ project 1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, Enter n1");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please, Enter n2");
+            int n2 = int.Parse(Console.ReadLine());
+            for (int i = n1; i < n2; i++)
+            {
+                bool isPrime = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+
+        }
+    }
+}
